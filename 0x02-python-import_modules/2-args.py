104fcd08916a6cad0475dd7cd9902dee090e02e0
Enoch Taylor <tay121222@gmail.com>
2023-06-14 13:08:30 +0000

    program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
 
     if length == 1:
         print("{:d} arguments.".format(length - 1))
+    elif length == 2:
+        print("{:d} argument:".format(length - 1))
     elif length > 1:
         print("{:d} arguments:".format(length - 1))
         for i in range(1, length):
