504efd8227870d3b8d56563ac3f2e56d814fff12
Enoch Taylor <tay121222@gmail.com>
2023-06-14 18:04:46 +0000

    program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
+
 
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
