eca223a04cc0ff3136ac65956fd92a18fd3de73c
Enoch Taylor <tay121222@gmail.com>
2023-06-14 13:47:27 +0000

    program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    names.sort()
+    for name in names:
+        print(name)
