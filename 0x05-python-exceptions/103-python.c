3cc5c219e16c2f2904ee99b3d698cfcf8349f660
Enoch Taylor <tay121222@gmail.com>
2023-07-04 15:40:11 +0000

    function that does exactly the same as the following Python bytecode

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -14,7 +14,7 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t size = ((PyVarObject *)p)->ob_size;
 	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
 
 	printf("[*] Python list info\n");
@@ -23,7 +23,7 @@ void print_python_list(PyObject *p)
 
 	for (Py_ssize_t i = 0; i < size; i++)
 	{
-		PyObject *item = PyList_GetItem(p, i);
+		PyObject *item = ((PyListObject *)p)->ob_item[i];
 		const char *item_type = Py_TYPE(item)->tp_name;
 
 		printf("Element %ld: %s\n", i, item_type);
@@ -43,16 +43,17 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	Py_ssize_t size = PyBytes_Size(p);
+	Py_ssize_t size = PyBytes_GET_SIZE(p);
+	const char *str = PyBytes_AsString(p);
 
 	printf("[.] bytes object info\n");
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", PyBytes_AsString(p));
+	printf("  trying string: %s\n", str);
 
 	printf("  first 10 bytes: ");
 	for (Py_ssize_t i = 0; i < size && i < 10; i++)
 	{
-		printf("%02x ", PyBytes_AsString(p)[i]);
+		printf("%02x ", (unsigned char)str[i]);
 	}
 	printf("\n");
 }
@@ -70,7 +71,7 @@ void print_python_float(PyObject *p)
 		return;
 	}
 
-	double value = PyFloat_AsDouble(p);
+	double value = PyFloat_AS_DOUBLE(p);
 
 	printf("[.] float object info\n");
 	printf("  value: %f\n", value);
