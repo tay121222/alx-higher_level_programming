cfb89060d0950a33bcc178ece1f6750c5966caa0
Enoch Taylor <tay121222@gmail.com>
2023-07-09 00:20:42 +0000

    minor changes to check m_a and m_b can't be empty

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -17,10 +17,10 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(element, list) for element in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    if len(m_a) == 0:
+    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
         raise ValueError("m_a can't be empty")
 
-    if len(m_b) == 0:
+    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
         raise ValueError("m_b can't be empty")
 
     if any(not isinstance(element, (int, float))
@@ -39,7 +39,7 @@ def matrix_mul(m_a, m_b):
     if len(set(len(element) for element in m_b)) != 1:
         raise TypeError("each row of m_b must be of the same size")
 
-    if len(m_a[0]) != len(m_b):
+    if len(m_a[0]) != len(m_b) or len(m_b[0]) != len(m_a):
         raise ValueError("m_a and m_b can't be multiplied")
 
     r1 = []
