f97ebecfaaf40326a1b34e022cb1151e474b8d2c
Enoch Taylor <tay121222@gmail.com>
2023-07-10 15:46:43 +0000

    class Rectangle that defines a rectangle by: (based on 8-rectangle.py)x

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -82,4 +82,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """returns a new Rectangle instance"""
-        return cls(size, size)
+        return Rectangle(size, size)
