6b3ebdec5f6b6f98db01e29e284a8ec657335301
Enoch Taylor <tay121222@gmail.com>
2023-07-11 14:02:36 +0000

    class BaseGeometry (based on 6-base_geometry.py)

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Empty Class"""
+
+
+class BaseGeometry:
+    """An empty Class"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """that validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value<= 0:
+            raise ValueError("{} must be greater than 0".format(name))
