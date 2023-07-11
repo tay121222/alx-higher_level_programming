df3015cec4b3b9e879a655c62ea094126345e83e
Enoch Taylor <tay121222@gmail.com>
2023-07-11 13:41:14 +0000

    class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty Class"""
+
+
+class BaseGeometry:
+    """An empty Class"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
