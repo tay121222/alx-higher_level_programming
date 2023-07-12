b5a3e93e20d407eb7ed245be10a756ac589578e5
Enoch Taylor <tay121222@gmail.com>
2023-07-12 05:24:58 +0000

    function that creates an Object from a \“JSON file\”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""contains function that creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”"""
+    with open(filename, "r") as file:
+        jsonate = json.load(file)
+        return jsonate
