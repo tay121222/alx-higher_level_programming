f4c3bff7bf9d29c383811b5e87376bd5b42e769a
Enoch Taylor <tay121222@gmail.com>
2023-07-12 10:28:05 +0000

    class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""contains class Student that defines a student"""
+
+
+class Student:
+    """class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializing variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation
+        of a Student instance based on attributes
+        passed to it
+        """
+        obj_list = self.__dict__
+        new_list = dict()
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            for a in attrs:
+                if a in obj_list:
+                    new_list.update({a: self.__dict__[a]})
+            return new_list
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        """replaces attributes of the Student instance"""
+        for attri in json:
+            self__dict__[attri] = json[attri]
