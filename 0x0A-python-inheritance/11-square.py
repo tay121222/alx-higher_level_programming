8d808028f5bbdc71eb2c500c89c0682522552f05
Enoch Taylor <tay121222@gmail.com>
2023-07-14 13:06:07 +0000

    class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains Rectangle that inherits from BaseGeometry"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits from a Rectangle"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Returns an area of rectangle"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """returns the square description"""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
