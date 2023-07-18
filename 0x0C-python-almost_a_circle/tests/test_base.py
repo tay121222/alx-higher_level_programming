86e17f1886aea18d1a001776942a6366dab5336a
Enoch Taylor <tay121222@gmail.com>
2023-07-18 11:07:53 +0000

    Create a folder named models with an empty file __init__.py

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Testing Base.py"""
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """Test Cases"""
+
+    def test_base_creation(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_base_with_id(self):
+        b3 = Base(100)
+        self.assertEqual(b1.id, 100)
+
+    def test_base_with_id(self):
+        b5 = Base()
+        b6 = Base()
+        self.assertEqual(b6.id, b5.id + 1)
+
+    def test_base_with_id_increment(self):
+        b1 = Base(12)
+        b2 = Base(1)
+        self.assertEqual(b1.id + b2.id, 13)
+
+if __name__ == "__main__":
+    unittest.main()
