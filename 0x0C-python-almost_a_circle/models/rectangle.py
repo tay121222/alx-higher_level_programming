d79270a5d72d618767484f6b79f317d29b363ebe
Enoch Taylor <tay121222@gmail.com>
2023-07-19 13:52:00 +0000

    Update the class Rectangle by improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -71,8 +71,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints visual rep of the rectangle to stdout"""
+        for j in range(self.__y):
+            print("")
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
