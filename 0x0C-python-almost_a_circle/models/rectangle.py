a81f47bde61c388b5c79438a1eb599286956eb6b
Enoch Taylor <tay121222@gmail.com>
2023-07-19 11:29:35 +0000

    class Rectangle by overriding the __str__ method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,9 @@ class Rectangle(Base):
         """Prints visual rep of the rectangle to stdout"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height
+                )
