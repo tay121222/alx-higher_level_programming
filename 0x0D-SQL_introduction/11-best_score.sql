643398c265133b008a28272d3ba22d15fae7654c
Enoch Taylor <tay121222@gmail.com>
2023-08-16 05:59:42 +0000

    correction to task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- ists all records with a score >= 10 in the table second_table
-select score, name from second_table where score >= 10 order by score desc;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
