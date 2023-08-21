dd045a18314359ecad77f6e6ca2953310d02cd8b
Enoch Taylor <tay121222@gmail.com>
2023-08-21 00:09:29 +0000

    script that lists all shows from hbtn_0d_tvshows_rate by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating
+SELECT tv_shows.title, SUM(rate) as rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC
