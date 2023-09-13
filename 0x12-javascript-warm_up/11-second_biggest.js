7bc29733c583fe5d7168c2d78ea5a3a43507a5cb
Enoch Taylor <tay121222@gmail.com>
2023-09-13 23:22:55 +0000

    script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,13 +2,14 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const temp = parseInt(process.argv[2]);
+  let temp = parseInt(process.argv[2]);
   let temp2 = parseInt(process.argv[3]);
 
   for (let i = 4; i < process.argv.length; i++) {
     const current = parseInt(process.argv[i]);
     if (current > temp) {
       temp2 = temp;
+      temp = current;
     } else if (current > temp2 && current !== temp) {
       temp2 = current;
     }
