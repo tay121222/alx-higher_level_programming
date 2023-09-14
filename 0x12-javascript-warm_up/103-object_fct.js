603cd8327ae3b4959b2ac8ecc8ca35a39817f25a
Enoch Taylor <tay121222@gmail.com>
2023-09-14 11:30:16 +0000

    script by adding a new function incr that increments the integer value

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,8 +5,8 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => {
-	this.value += 1;
+myObject.incr = function () {
+  this.value += 1;
 };
 myObject.incr();
 console.log(myObject);
