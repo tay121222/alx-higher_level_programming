9e42270f2ee5543c9ca79c6ec3ef2801bfd3fc48
Enoch Taylor <tay121222@gmail.com>
2023-09-17 19:57:09 +0000

    script that lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,9 +8,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute(
-            "SELECT * FROM states WHERE LEFT(name, 1) = 'N'"
-            )
+    cur.execute("SELECT * FROM states WHERE BINARY LEFT(name, 1) = 'N'")
     states = cur.fetchall()
     for name_N in states:
         print(name_N)
