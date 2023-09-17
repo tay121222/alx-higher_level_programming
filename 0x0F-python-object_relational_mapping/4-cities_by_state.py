b33e0d857d73030da6f2e52e63f6edd26e518401
Enoch Taylor <tay121222@gmail.com>
2023-09-17 20:30:07 +0000

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all cities
+from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = (
+            """SELECT cities.id, cities.name, states.name FROM cities
+            LEFT JOIN states ON states.id = cities.state_id
+            ORDER BY cities.id ASC
+            """
+            )
+    cur.execute(query)
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
