c6ab1b65570b1e6227b6223e8ed1069c3b35a20e
Enoch Taylor <tay121222@gmail.com>
2023-09-17 20:59:54 +0000

    script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an
+argument and lists all cities
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = """SELECT TRIM(TRAILING ', ' FROM GROUP_CONCAT(name SEPARATOR ', '))
+            FROM cities WHERE state_id = (SELECT id from states WHERE name = %s)
+            """
+    cur.execute(query, (sys.argv[4],))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+    cur.close
+    db.close
