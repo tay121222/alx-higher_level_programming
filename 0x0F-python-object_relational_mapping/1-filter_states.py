c8feb276a982c6b4563b53af848a678ec994c959
Enoch Taylor <tay121222@gmail.com>
2023-09-17 19:06:12 +0000

    script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+            """SELECT * FROM states WHERE SUBSTRING(name, 1, 1) = 'N'
+            ORDER BY id DESC"""
+            )
+    states = cur.fetchall()
+    for name_N in states:
+        print(name_N)
+    db.close
