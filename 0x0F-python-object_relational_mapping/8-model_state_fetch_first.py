ddf1962b87c7dcc4b73fbd5593b8627925b2e0c9
Enoch Taylor <tay121222@gmail.com>
2023-09-18 15:15:47 +0000

    Adding solution files for task 7 and 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that prints the first State object
+from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(
+            f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    session.close()
