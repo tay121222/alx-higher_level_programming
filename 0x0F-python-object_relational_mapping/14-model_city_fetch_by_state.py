c40d925458f02e4f90cd99ae9522ba7c1e95b46d
Enoch Taylor <tay121222@gmail.com>
2023-09-19 11:49:21 +0000

    Python file similar to model_state.py named model_city.py that contains the class definition of a City

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script to print all City objects from
+the hbtn_0e_14_usa database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+from model_state import State
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine(
+            f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City, State).join(State, City.state_id == State.id).all()
+
+    for city, state in cities:
+        print("{}: {} {}".format(state.name, city.id, city.name))
+    session.close()
