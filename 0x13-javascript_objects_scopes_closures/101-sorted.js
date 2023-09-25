5626feb24d72c2a672c568c5d2a02183277ad79e
Enoch Taylor <tay121222@gmail.com>
2023-09-25 23:08:35 +0000

    script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const userId in dict) {
+  const occurrence = dict[userId];
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [];
+  }
+  newDict[occurrence].push(userId);
+}
+
+console.log(newDict);
