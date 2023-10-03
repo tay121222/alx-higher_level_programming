e9d5b632f9e3a6e60eaf6a33dcaae2b6bfaf75c5
Enoch Taylor <tay121222@gmail.com>
2023-10-03 11:26:13 +0000

    updated

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -s -X PUT 0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=98" -H "Origin: School" -L 0.0.0.0:5000/catch_me
