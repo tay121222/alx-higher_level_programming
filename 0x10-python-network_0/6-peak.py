d303650c2efa4ea48a15c654a7f67b50e46d7111
Enoch Taylor <tay121222@gmail.com>
2023-10-03 10:39:52 +0000

    updated a bit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,7 +11,7 @@ def find_peak(list_of_integers):
     right = len(list_of_integers) - 1
 
     while left <= right:
-        mid = (left + right)  // 2
+        mid = (left + right) // 2
 
         if (
                 mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]
