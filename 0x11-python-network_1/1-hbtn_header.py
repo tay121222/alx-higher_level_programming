d534681a77fad6f7124d3ae18aa8cf7f7267808c
Enoch Taylor <tay121222@gmail.com>
2023-10-10 23:09:20 +0000

    Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response updated

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request
-to the URL and displays the value of the X-Request-Id variable
-"""
+"""Python script that takes in a URL, sends a request"""
 import urllib.request
 import sys
 
