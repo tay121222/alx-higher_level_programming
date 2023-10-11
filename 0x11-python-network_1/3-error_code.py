10f13a71dbcee09894db6fd6300262eda72e14f3
Enoch Taylor <tay121222@gmail.com>
2023-10-11 19:50:56 +0000

    Python script that takes in a URL, sends a request to the URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request
+to the URL and displays the body of the response"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
