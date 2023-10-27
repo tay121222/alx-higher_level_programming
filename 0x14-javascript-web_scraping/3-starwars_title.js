fbef483b00147a4507a97141284542ee172be7ec
Enoch Taylor <tay121222@gmail.com>
2023-10-27 15:28:09 +0000

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(`Title: ${data.title}`);
+  } else {
+    console.error(error);
+  }
+});
