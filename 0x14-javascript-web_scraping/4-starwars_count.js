ed893a0d82c0ca3b355ac2e3e86b556ee2ce59c4
Enoch Taylor <tay121222@gmail.com>
2023-10-30 00:37:16 +0000

    Star wars Wedge Antilles - updated

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,14 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18;
-let count = 0;
+const wedgeAntillesId = 18;
 
-request(apiUrl, function (error, response, body) {
+request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const films = JSON.parse(body).results;
-    films.forEach((film) => {
-      const characters = film.characters;
-      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
+    const count = films.reduce((acc, film) => {
+      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`) ? acc + 1 : acc;
+    }, 0);
     console.log(count);
   } else {
     console.error(error);
