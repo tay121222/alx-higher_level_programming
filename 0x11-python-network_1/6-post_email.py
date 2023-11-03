81d46388c4019e0d933c45d5e069dbf17fba0dfe
Enoch Taylor <tay121222@gmail.com>
2023-11-03 11:39:00 +0000

    Python script that takes in a URL and an email address, sends a POST request to the passed URL

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data_ok = {'email': email}
+    response = requests.post(url, data=data_ok)
+    print(response.text)
