8e65a5e52a41c33c0e9e148eb7cbd1952d1f687e
Enoch Taylor <tay121222@gmail.com>
2023-11-08 13:01:40 +0000

    JavaScript script that updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+            var header = document.querySelector("header");
+            if (header) {
+                header.style.color = "#FF0000";
+            }
+        });
